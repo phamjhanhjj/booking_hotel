cc5308f89429e248a50c4cd4a3ca2bcd8514c84d
phamjhanhj <22010503@st.phenikaa-uni.edu.vn>
2024-10-01 13:22:18 +0700

    main

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+
+import 'package:booking_hotel/providers/user_provider.dart';
+import 'package:flutter/material.dart';
+
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'screens/login_signup/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:flutter_config/flutter_config.dart';
+
+final userProvider = ChangeNotifierProvider<UserProvider>((ref) {
+  return UserProvider();
+});
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  await FlutterConfig.loadEnvVariables();
+  runApp(
+    ProviderScope(
+      overrides: [
+        userProvider,
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: ThemeMode.system,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      home: const LoginScreen(),
+    );
+ }
+}
