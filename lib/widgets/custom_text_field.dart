355cf72cd00e8e2206d667e1d24ee59af54f9870
phamjhanhj <22010503@st.phenikaa-uni.edu.vn>
2024-10-02 15:21:08 +0700

    main

--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -1,1 +1,28 @@
-// TODO Implement this library.
+import 'package:flutter/material.dart';
+import 'app_text.dart';
+
+class CustomTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String label;
+  
+  const CustomTextField({
+    Key? key,
+    required this.controller,
+    required this.label,
+
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          label:AppText.small(label, color: Colors.blue),
+          border: InputBorder.none,
+        ),
+        style: const TextStyle(fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
