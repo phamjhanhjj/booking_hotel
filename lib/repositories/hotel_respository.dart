add71285c4ef753b0c130115bd2e9c9f21da5e14
lingng04 <nguyenlplinh2004@gmail.com>
2024-10-02 14:25:02 +0700

    hotel_respository

--- lib/repositories/hotel_respository.dart
+++ lib/repositories/hotel_respository.dart
@@ -1,1 +1,18 @@
-// TODO Implement this library.
+import 'package:booking_hotel/models/hotel_model.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'hotel_respository.g.dart';
+
+@riverpod
+HotelRespository hotelRespository(HotelRespositoryRef ref) => HotelRespository();
+
+class HotelRespository {
+  Future<List<HotelModel>> getHotels() async {
+    return await parseJson();
+  }
+
+  Future<HotelModel> getHotel(String hotelId) async {
+    List<HotelModel> hotels = await getHotels();
+    return hotels.firstWhere((hotel) => hotel.id == hotelId);
+  }
+}
---
+++ lib/repositories/hotel_respository.g.dart
@@ -0,0 +1,22 @@
+part of 'hotel_respository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$hotelRespositoryHash() => r'af910851d2e8a0b5cb26d0ff2cbf0a30f0422d82';
+
+/// See also [hotelRespository].
+@ProviderFor(hotelRespository)
+final hotelRespositoryProvider = AutoDisposeProvider<HotelRespository>.internal(
+  hotelRespository,
+  name: r'hotelRespositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$hotelRespositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef HotelRespositoryRef = AutoDisposeProviderRef<HotelRespository>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
