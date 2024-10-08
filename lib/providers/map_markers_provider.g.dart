385b792398c5592569f249bff91599f7a13bac16
lingng04 <nguyenlplinh2004@gmail.com>
2024-10-08 09:45:30 +0700

    map

---
+++ lib/providers/map_markers_provider.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'map_markers_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$mapMarkersHash() => r'c5f171cd4301230d31730746a6829c3889815a62';
+
+/// See also [MapMarkers].
+@ProviderFor(MapMarkers)
+final mapMarkersProvider =
+    AutoDisposeAsyncNotifierProvider<MapMarkers, Set<Marker>>.internal(
+  MapMarkers.new,
+  name: r'mapMarkersProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$mapMarkersHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$MapMarkers = AutoDisposeAsyncNotifier<Set<Marker>>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
